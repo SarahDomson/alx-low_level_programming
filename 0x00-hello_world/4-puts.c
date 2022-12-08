16755998189deef4f804e227c1f2bd539d7bb420
SarahDomson <sarahdomson26@gmail.com>
2022-12-08 13:03:10 -0800

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - starting  point of your code
+ *
+ * out put: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 if Successfull
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
