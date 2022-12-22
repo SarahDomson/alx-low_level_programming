93a66493c7a9c08344036931fdf080566083128f
SarahDomson <sarahdomson26@gmail.com>
2022-12-22 00:19:18 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,35 +1,23 @@
 #include "main.h"
 /**
- * cap_string - capitalizes every first letter of a word in a string
- * separators of words are: space, tabulation
- * new line
- * @s: POinter to string
+ * string_toupper - changes all lowercase letters of  a string to uppercase
+ * @word: Pointer to string
  *
- * Return: Pointer to s
+ * Return: Pointer to uppercase string
  */
-char *cap_string(char *s)
+char *string_toupper(char *word)
 {
-	int count;
+	int length;
 
-	count = 0;
-  while (s[count] != '\0')
+	length = 0;
+
+	while (word[length] != '\0')
 	{
-		if (s[0] >= 97 && s[0] <= 122)
+		if (word[length] >= 97 && word[length] <= 122)
 		{
-			s[0] = s[0] - 32;
+			word[length] = word[length] - 32;
 		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
-		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
-		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
-		    || s[count] == '{' || s[count] == '}')
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
-			{
-				s[count + 1] = s[count + 1] - 32;
-			}
-		}
-		count++;
+		length++;
 	}
-	return (s);
+	return (word);
 }
